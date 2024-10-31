e1936ef65a73470639a3934a5f25bd9e9b271a59
joshua-s <sepulvedaj1@mail.montclair.edu>
2024-10-31 15:11:49 -0400

    footer

---
+++ footer.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <style>
+            ul{
+                list-style-type: none;
+            }
+        </style>
+    </head>
+  <body>
+    <footer class="bg-light text-center text-lg-start">
+      <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        Copyright © 2024 Virtual Resort
+        <ul>
+            <li>
+                <a href="mailto:sepulvedaj1@montclair.edu">Joshua Sepulveda</a>
+            </li>
+            <li>
+                <a href="mailto:amorea1@montclair.edu">Antonio Amore</a>
+            </li>
+        </ul>
+      </div>
+    </footer>
+  </body>
+</html>
