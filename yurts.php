891f995207c1e550bdf302087a5571e0826c4bd5
Antonio Amore <aamore2003@gmail.com>
2024-10-31 15:01:13 -0400

    update navbar and templates

--- yurts.php
+++ yurts.php
@@ -5,10 +5,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Yurts - Virtual Resort</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   </head>
   <body>
-    <h1>Virtual Resort</h1>
-
+    <h1 class="display-6 text-center">Virtual Resort</h1>
+    <nav class="navbar navbar-expand-lg bg-success">
+  <div class="container">
+    <a class="navbar-brand" href="/">
+      <i class="bi bi-buildings"></i>
+      Virtual Resort
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/yurts.php">Yurts</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/activities.php">Activities</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/reservation.php">Reservation</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/comments.php">Comments</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
