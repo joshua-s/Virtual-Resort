c0a1c0352efdd55d56cf1ba3e1704c8616270031
joshua-s <sepulvedaj1@mail.montclair.edu>
2024-10-31 21:14:39 -0400

    forms

--- reservation.php
+++ reservation.php
@@ -39,7 +39,65 @@
     </div>
   </div>
 </nav>
-
+<div class="container form-container">
+        <h1>Reservation at Virtual Resort</h1>
+        <form>
+            <div class="form-group row">
+                <label for="firstName" class="col-12 col-sm-4 col-form-label">First Name:</label>
+                <div class="col-12 col-sm-8">
+                    <input type="text" class="form-control" id="firstName" name="firstName" required>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="lastName" class="col-12 col-sm-4 col-form-label">Last Name:</label>
+                <div class="col-12 col-sm-8">
+                    <input type="text" class="form-control" id="lastName" name="lastName" required>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="addressNumber" class="col-12 col-sm-4 col-form-label">Number & Street:</label>
+                <div class="col-12 col-sm-8">
+                    <input type="text" class="form-control" id="numberStreet" name="numberStreet" required>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="city" class="col-12 col-sm-4 col-form-label">City:</label>
+                <div class="col-12 col-sm-8">
+                    <input type="text" class="form-control" id="city" name="city" required>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="state" class="col-12 col-sm-4 col-form-label">State:</label>
+                <div class="col-12 col-sm-8">
+                    <input type="text" class="form-control" id="state" name="state" required>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="state" class="col-12 col-sm-4 col-form-label">Zip Code:</label>
+                <div class="col-12 col-sm-8">
+                    <input type="zip" class="form-control" id="zip" name="zip" required>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="reservationDate" class="col-12 col-sm-4 col-form-label">Reservation Date:</label>
+                <div class="col-12 col-sm-8">
+                    <input type="date" class="form-control" id="reservationDate" name="reservationDate" required>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="comments" class="col-12 col-sm-4 col-form-label">Comments:</label>
+                <div class="col-12 col-sm-8">
+                    <textarea class="form-control" id="comments" name="comments" rows="3"></textarea>
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-12 col-sm-8 offset-sm-4">
+                    <button type="submit" class="btn btn-primary">Reserve Now</button>
+                    <button type="reset" class="btn btn-success">Reset</button>
+                </div>
+            </div>
+        </form>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
